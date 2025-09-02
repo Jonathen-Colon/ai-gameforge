54ce9fb42fbecaab07d6850ae6dccc9f3c2fedc7
Jonathen Colon <jonathen@joncolon.dev>
2025-09-02 00:16:24 -0400

    nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import "@/global.css";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-black-500">
+        Welcome to NativeWind!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ffffff",
-  },
-  title: {
-    fontSize: 20, // Tailwind text-xl ≈ 20
-    fontWeight: "700",
-    color: "#3B82F6", // Tailwind blue-500
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Paths to scan for Tailwind classes (web usage only)
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
