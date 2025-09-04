11d7d3679a6e97365c7343e67a3200e129157281
Jonathen Colon <jonathen@joncolon.dev>
2025-09-04 07:15:04 -0400

    added convex

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
+import { api } from "@/convex/_generated/api";
 import "@/global.css";
+import { useQuery } from "convex/react";
 import { Text, View } from "react-native";
 
 export default function App() {
+  const tasks = useQuery(api.tasks.get);
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-black-500">
-        Welcome to NativeWind!
-      </Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      {tasks?.map(({ _id, text }) => <Text className="text-xl font-bold text-white" key={_id}>{text}</Text>)}
     </View>
   );
 }
